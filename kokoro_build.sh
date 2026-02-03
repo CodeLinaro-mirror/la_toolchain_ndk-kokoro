462c2bd411d2699874e598181f3a404b83ee5a88
Colin Cross <ccross@android.com>
2026-02-03 10:48:16 -0800

    Add a kokoro_build.sh that can be shared by cmake, ninja and python
    Add a kokoro_build.sh that does the hard work of starting a docker
    container that may need to be a sibling container to a kokoro
    container.
    
    Bug: 474214382
    Test: KOKORO_HOST_ROOT_DIR=$PWD KOKORO_ROOT_DIR=/tmp/build docker run -eKOKORO_HOST_ROOT_DIR -eKOKORO_ROOT_DIR -v/var/run/docker.sock:/var/run/docker.sock -v$PWD:/tmp/build --entrypoint /tmp/build/external/ninja/kokoro/kokoro_build.sh ndk-docker
    Change-Id: Icf1f566dd9b6bb695dab2f2c8776d5d87b1bccf8

---
+++ kokoro_build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+set -x
+
+top=$(cd $(dirname $0)/../.. && pwd)
+docker_dir=$1
+entry_point=$2
+
+
+# On Linux, enter the Docker container before invoking the entry point.  If running in a Kokoro
+# instances build this script may be run from inside a Docker container, but with the host
+# docker.sock mounted to allow creating sibling containers.  In that case, use Kokoro's
+# environment variables to translate the in-container paths back to the host paths necessary
+# to create the sibling container.
+if [ "$(uname)" == "Linux" -a "$SKIP_DOCKER" == "" ]; then
+    if [ -n "${KOKORO_HOST_ROOT_DIR}" ]; then
+        host_top=${KOKORO_HOST_ROOT_DIR}/$(realpath --relative-to="${KOKORO_ROOT_DIR}" $top)
+    else
+        host_top=$top
+    fi
+    container_build_root=/build
+    entry_point_in_container=${container_build_root}/$(realpath --relative-to="$top" $entry_point)
+    docker_image=ndk-kokoro
+    docker build -t $docker_image $docker_dir
+    export SKIP_DOCKER=1
+    docker run -v$host_top:$container_build_root -eKOKORO_BUILD_ID -eSKIP_DOCKER \
+      --entrypoint $entry_point_in_container \
+      $docker_image
+    exit $?
+fi
+
+$entry_point
